6d52e9e7e7c787b26d65f455193429430d090053
Thathokuhle <simthandathathokuhle@gmail.com>
2025-05-13 10:07:09 +0200

    Just fix DataContext, left is FromSqlRaw error

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Ganss.Xss;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nest;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UlundiManagement.Business.Student;
+using UlundiManagement.Data;
+using UlundiManagement.Repository.IRepositories;
+using UlundiManagement.Repository.Repositories;
+using UlundiManagement.ViewModels.Student;
+
+namespace UlundiManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        public ActionResult Index(string searchName)
+        {
+            searchName = new HtmlSanitizer().Sanitize(searchName);
+            var students = new StudentBusiness().AllStudent(searchName);
+            return View(students);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
+using AutoMapper.Internal.Mappers;
+using FormsManagement.Business.MappingBusiness;
+using Microsoft.EntityFrameworkCore;
+using UlundiManagement.Business.Student;
+using UlundiManagement.Data;
+using UlundiManagement.Repository.IRepositories;
+using UlundiManagement.Repository.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Connect to database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<StudentBusiness>();
+
+
+builder.Services.AddAutoMapper(typeof(ObjectMapper));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
